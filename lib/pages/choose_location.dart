4447a1c0c689c87ff6d3b80fb47450b1a05f7ba3
Harsh <harshgupta1249@gmail.com>
2021-08-04 09:39:31 +0530

    initState

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -6,10 +6,33 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+
+  int counter = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    print('init ran');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('choose location'),
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[800],
+        title: Text('Choose a location'),
+        centerTitle: true,
+        elevation:0,
+      ),
+      body: ElevatedButton(
+        onPressed: (){
+          setState((){
+            counter += 1;
+          });
+        },
+        child:Text('$counter'),
+      ),
     );
   }
 }
