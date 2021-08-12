11e41b726da79fb0a6b81a21b0cfcfc1ec7a3f4d
Harsh <harshgupta1249@gmail.com>
2021-08-12 20:32:17 +0530

    intl package

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -8,15 +8,16 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
-  String time = 'loading';
 
   void setupWorldTime() async {
     
     WorldTime instance = WorldTime(location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
     await instance.getTime();
-    print(instance.time);
-    setState((){
-      time=instance.time;
+
+    Navigator.pushReplacementNamed(context, '/home', arguments: {
+      'location' : instance.location,
+      'flag' : instance.flag,
+      'time' : instance.time
     });
   }
 
@@ -31,7 +32,7 @@ class _LoadingState extends State<Loading> {
     return Scaffold(
       body: Padding(
         padding: EdgeInsets.all(50.0),
-        child:Text(time),
+        child:Text('loading'),
       ),
     );
   }
